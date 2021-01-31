f4cc8e257ac2538efa3a408f4dd3e526ecd091d5
DESKTOP-7GDUUR0\liwenli <214132049@qq.com>
2021-01-31 10:18:50 +0800

    feat: 图片自适应

--- client/src/pages/group/index.jsx
+++ client/src/pages/group/index.jsx
@@ -4,5 +4,5 @@ import {Image} from '@tarojs/components'
 import './index.scss'
 
 export default function GroupIndex() {
-	return <Image className='qrcode' src='cloud://dev-4gfjzx0v3e4ac91a.6465-dev-4gfjzx0v3e4ac91a-1304440596/qrcode.jpg' />
+	return <Image className='qrcode' mode='widthFix' src='cloud://dev-4gfjzx0v3e4ac91a.6465-dev-4gfjzx0v3e4ac91a-1304440596/qrcode.jpg' />
 }
